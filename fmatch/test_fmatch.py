ff516c71ebec19644d6e8d486974cb261082567b
Shashank Reddy Boyapally <sboyapal@redhat.com>
2024-02-15 12:49:53 -0500

    Elasticsearch-dsl refactor, unit test, config update (#18)
    * refactor elasticsearch-dsl, update unit tests
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    * fixed pylint import error
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    * updated requirements.txt
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    * removed commented method
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    * removed debugs
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    ---------
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>

--- fmatch/test_fmatch.py
+++ fmatch/test_fmatch.py
@@ -1,53 +1,60 @@
 """
 test_fmatch
 """
+
 import sys
 # pylint: disable=import-error
 import pandas as pd
+
 # pylint: disable=import-error
 from matcher import Matcher
 
-match = Matcher(index='perf_scale_ci')
+match = Matcher(index="perf_scale_ci")
+res=match.get_metadata_by_uuid("b4afc724-f175-44d1-81ff-a8255fea034f",'perf_scale_ci')
 
 meta = {}
-meta['benchmark'] = "cluster-density-v2"
-meta['masterNodesType'] = "m6a.xlarge"
-meta['workerNodesType'] = "m6a.xlarge"
-meta['platform'] = "AWS"
-meta['masterNodesCount'] = 3
-meta['workerNodesCount'] = 24
-meta['jobStatus'] = "success"
-meta['ocpVersion'] = '4.15'
-meta['networkType'] = "OVNKubernetes"
-meta['encrypted'] = "true"
-meta['ipsec'] = "false"
-meta['fips'] = "false"
+meta["masterNodesType"] = "m6a.xlarge"
+meta["workerNodesType"] = "m6a.xlarge"
+meta["platform"] = "AWS"
+meta["masterNodesCount"] = 3
+meta["workerNodesCount"] = 24
+meta["jobStatus"] = "success"
+meta["ocpVersion"] = "4.15"
+meta["networkType"] = "OVNKubernetes"
+meta["benchmark"] = "cluster-density-v2"
+# meta['encrypted'] = "true"
+# meta['ipsec'] = "false"
+# meta['fips'] = "false"
 
 uuids = match.get_uuid_by_metadata(meta)
 if len(uuids) == 0:
     print("No UUID present for given metadata")
     sys.exit()
 runs = match.match_kube_burner(uuids)
+
 ids = match.filter_runs(runs, runs)
-podl = match.burner_results("", ids, "ripsaw-kube-burner*")
-
-kapi_cpu = match.burner_cpu_results(
-    ids, "openshift-kube-apiserver", "ripsaw-kube-burner*")
-ovn_cpu = match.burner_cpu_results(
-    ids, "openshift-ovn-kubernetes", "ripsaw-kube-burner*")
-etcd_cpu = match.burner_cpu_results(
-    ids, "openshift-etcd", "ripsaw-kube-burner*")
-ovn_mem = match.burner_mem_results(
-    ids, "openshift-ovn-kubernetes", "ripsaw-kube-burner*")
-
+podl_metrics = {
+    "name": "podReadyLatency",
+    "metricName": "podLatencyQuantilesMeasurement",
+    "quantileName": "Ready",
+    "metric_of_interest": "P99",
+    "not": {"jobConfig.name": "garbage-collection"},
+}
+podl = match.getResults("", ids, "ripsaw-kube-burner*",metrics=podl_metrics)
+kapi_metrics = {
+    "name": "apiserverCPU",
+    "metricName": "containerCPU",
+    "labels.namespace": "openshift-kube-apiserver",
+    "metric_of_interest": "value",
+    "agg": {"value": "cpu", "agg_type": "avg"},
+}
+kapi_cpu = match.get_agg_metric_query(ids, "ripsaw-kube-burner*", metrics=kapi_metrics)
 podl_df = match.convert_to_df(
     podl, columns=['uuid', 'timestamp', 'quantileName', 'P99'])
 kapi_cpu_df = match.convert_to_df(kapi_cpu)
 merge_df = pd.merge(kapi_cpu_df, podl_df, on="uuid")
 match.save_results(merge_df, "merged.csv", [
                    "uuid", "timestamp_x", "cpu_avg", "P99"])
-match.save_results(kapi_cpu_df, "CPUavg24.csv")
-match.save_results(podl_df, "podlatency24.csv")
 
 df = pd.read_csv("merged.csv")
 ls = df["uuid"].to_list()
