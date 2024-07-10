69145e467ef6e2f2c9a665fb087eeff1538820a2
Shashank Reddy Boyapally <sboyapal@redhat.com>
2024-07-10 14:46:40 -0400

    Date filter, logging and types (#26)
    * added logging, date filter and types
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    * added types for constructor
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    * add pull request to workflows, add new unit test
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    * pylint error
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    * bumped version
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    * updated utility for functional testing
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>
    
    ---------
    
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>

--- fmatch/test_fmatch.py
+++ fmatch/test_fmatch.py
@@ -2,24 +2,31 @@
 test_fmatch
 """
 
+from datetime import datetime
 import sys
+import warnings
 # pylint: disable=import-error
 import pandas as pd
 
 # pylint: disable=import-error
 from matcher import Matcher
 
-match = Matcher(index="perf_scale_ci")
-res=match.get_metadata_by_uuid("b4afc724-f175-44d1-81ff-a8255fea034f",'perf_scale_ci')
+warnings.filterwarnings("ignore", message="Unverified HTTPS request.*")
+warnings.filterwarnings(
+    "ignore", category=UserWarning, message=".*Connecting to.*verify_certs=False.*"
+)
+
+match = Matcher(index="perf_scale_ci*", verify_certs=False)
+res=match.get_metadata_by_uuid("b4afc724-f175-44d1-81ff-a8255fea034f",'perf_scale_ci*')
 
 meta = {}
 meta["masterNodesType"] = "m6a.xlarge"
 meta["workerNodesType"] = "m6a.xlarge"
 meta["platform"] = "AWS"
 meta["masterNodesCount"] = 3
-meta["workerNodesCount"] = 24
+meta["workerNodesCount"] = 6
 meta["jobStatus"] = "success"
-meta["ocpVersion"] = "4.15"
+meta["ocpVersion"] = "4.17"
 meta["networkType"] = "OVNKubernetes"
 meta["benchmark.keyword"] = "cluster-density-v2"
 # meta['encrypted'] = "true"
@@ -27,10 +34,15 @@ meta["benchmark.keyword"] = "cluster-density-v2"
 # meta['fips'] = "false"
 
 uuids = match.get_uuid_by_metadata(meta)
+print("All uuids",len(uuids))
+date= datetime.strptime("2024-07-01T13:46:24Z","%Y-%m-%dT%H:%M:%SZ")
+uuids2= match.get_uuid_by_metadata(meta,lookback_date=date)
+print("lookback uuids",len(uuids2))
+uuids2 = match.get_uuid_by_metadata(meta)
 if len(uuids) == 0:
     print("No UUID present for given metadata")
     sys.exit()
-runs = match.match_kube_burner(uuids)
+runs = match.match_kube_burner(uuids,"ripsaw-kube-burner*")
 
 ids = match.filter_runs(runs, runs)
 podl_metrics = {
--- fmatch/tests/test_matcher.py
+++ fmatch/tests/test_matcher.py
@@ -7,6 +7,7 @@ Unit Test file for fmatch.py
 # pylint: disable = import-error, duplicate-code
 import os
 from unittest.mock import patch
+import datetime
 
 from elasticsearch_dsl import Search
 from elasticsearch_dsl.response import Response
@@ -71,6 +72,29 @@ def test_get_uuid_by_metadata(matcher_instance):
     }
     result = matcher_instance.get_uuid_by_metadata(meta)
     expected = [{"uuid": "uuid1",
+                "buildUrl":"buildUrl1"},
+                {"uuid": "uuid2",
+                "buildUrl":"buildUrl1"}]
+    assert result == expected
+
+def test_get_uuid_by_metadata_lookback(matcher_instance):
+    matcher_instance.es.search = lambda *args, **kwargs: {
+        "hits": {
+            "hits": [{"_source": {"uuid": "uuid1",
+                                  "buildUrl":"buildUrl1",
+                                  "timestamp":"2024-07-10T13:46:24Z"}}, 
+                    {"_source": {"uuid": "uuid2",
+                                  "buildUrl":"buildUrl1",
+                                  "timestamp":"2024-07-08T13:46:24Z"}}]
+        }
+    }
+    meta = {
+        "field1": "value1",
+        "ocpVersion": "4.15",
+    }
+    date= datetime.datetime.strptime("2024-07-07T13:46:24Z","%Y-%m-%dT%H:%M:%SZ")
+    result = matcher_instance.get_uuid_by_metadata(meta=meta, lookback_date=date)
+    expected= [{"uuid": "uuid1",
                 "buildUrl":"buildUrl1"},
                 {"uuid": "uuid2",
                 "buildUrl":"buildUrl1"}]
