57d7a876ff6c176cd5dd8c0fadf1b83143afa6d9
Raul Sevilla <rsevilla@redhat.com>
2025-07-01 10:26:06 +0200

    Bump to 0.1.4
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup file for fmatch package
 from setuptools import setup, find_packages
 
 
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 DESCRIPTION = 'Common package for matching runs with provided metadata'
 # pylint: disable= line-too-long
 LONG_DESCRIPTION = "A package that allows to match metadata and get runs and create csv files with queried metrics"
