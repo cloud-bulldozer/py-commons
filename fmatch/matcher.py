f98ae5745fbf4dd02298e8db0c107b3da9bbdd31
Joe Talerico (rook) <joe.talerico@gmail.com>
2025-07-23 15:27:27 -0400

    Adding version to the response
    Need to capture the version for more AI fun!
    
    Signed-off-by: Joe Talerico (rook) <joe.talerico@gmail.com>

--- fmatch/matcher.py
+++ fmatch/matcher.py
@@ -18,67 +18,62 @@ from fmatch.logrus import SingletonLogger
 
 
 class Matcher:
+    """Matcher"""
     # pylint: disable=too-many-instance-attributes
-    """
-    A class used to match or interact with an Elasticsearch index for performance scale testing.
-
-    Attributes:
-        index (str): Name of the Elasticsearch index to interact with.
-        level (int): Logging level (e.g., logging.INFO).
-        es_url (str): Elasticsearch endpoint, can be specified by the environment variable ES_SERVER
-        verify_certs (bool): Whether to verify SSL certificates when connecting to Elasticsearch.
-        version_field (str): Name of the field containing the OpenShift version.
-        uuid_field (str): Name of the field containing the UUID.
-    """
-
     def __init__(
         self,
         index: str = "ospst-perf-scale-ci",
         level: int = logging.INFO,
-        es_url: str = os.getenv("ES_SERVER"),
+        ES_URL: str = os.getenv("ES_SERVER"),
         verify_certs: bool = True,
         version_field: str = "ocpVersion",
         uuid_field: str = "uuid"
     ):
         self.index = index
+        self.es_url = ES_URL
         self.search_size = 10000
         self.logger = SingletonLogger(debug=level, name="Matcher")
-        self.es = Elasticsearch([es_url], timeout=30, verify_certs=verify_certs)
+        self.es = Elasticsearch([self.es_url], timeout=30, verify_certs=verify_certs)
         self.data = None
         self.version_field = version_field
         self.uuid_field = uuid_field
 
-    def get_metadata_by_uuid(self, uuid: str) -> dict:
+    def get_metadata_by_uuid(self, uuid: str, index: str = None) -> dict:
         """Returns back metadata when uuid is given
 
         Args:
             uuid (str): uuid of the run
+            index (str, optional): index to be searched in. Defaults to None.
 
         Returns:
             _type_: _description_
         """
+        if index is None:
+            index = self.index
         query = Q("match",  **{self.uuid_field: {"value": f"{uuid}"}})
         result = {}
-        s = Search(using=self.es, index=self.index).query(query)
-        res = self.query_index(s)
+        s = Search(using=self.es, index=index).query(query)
+        res = self.query_index(index, s)
         hits = res.hits.hits
         if hits:
             result = dict(hits[0].to_dict()["_source"])
         return result
 
-    def query_index(self, search: Search) -> Response:
+    def query_index(self, index: str, search: Search) -> Response:
         """generic query function
 
         Args:
+            index (str): _description_
             search (Search) : Search object with query
         """
-        self.logger.info("Executing query against index: %s", self.index)
+        self.logger.info("Executing query against index=%s", index)
         self.logger.debug("Executing query \r\n%s", search.to_dict())
         return search.execute()
 
     def get_uuid_by_metadata(
         self,
         meta: Dict[str, Any],
+        index: str = None,
         lookback_date: datetime = None,
         lookback_size: int = 10000,
         timestamp_field: str = "timestamp"
@@ -87,30 +82,37 @@ class Matcher:
 
         Args:
             meta (Dict[str, Any]): metadata of the runs
-            lookback_date (datetime, optional):
+            index (str, optional): Index to search. Defaults to None.
+            lookback_date (datetime, optional): 
             The cutoff date to get the uuids from. Defaults to None.
-            lookback_size (int, optional):
+            lookback_size (int, optional): 
             Maximum number of runs to get, gets the latest. Defaults to 10000.
 
-            lookback_size and lookback_date get the data on the
+            lookback_size and lookback_date get the data on the 
             precedency of whichever cutoff comes first.
             Similar to a car manufacturer's warranty limits.
 
         Returns:
             List[Dict[str, str]]: _description_
         """
-        must_clause = []
         must_not_clause = []
+        if index is None:
+            index = self.index
+
         version = str(meta[self.version_field])[:4]
 
-        for field, value in meta.items():
-            if field in ["ocpVersion", "ocpMajorVersion"]:
-                continue
-            if field != "not":
-                must_clause.append(Q("match", **{field: str(value)}))
-            else:
-                for not_field, not_value in meta["not"].items():
-                    must_not_clause.append(Q("match", **{not_field: str(not_value)}))
+        must_clause = [
+            (
+                Q("match", **{field: str(value)})
+                if isinstance(value, str)
+                else Q("match", **{field: value})
+            )
+            for field, value in meta.items()
+            if field not in [self.version_field, "ocpMajorVersion", "not"]
+        ]
+
+        for field, value in meta.get("not", {}).items():
+            must_not_clause.append(Q("match", **{field: str(value)}))
 
         if "ocpMajorVersion" in meta:
             version = meta["ocpMajorVersion"]
@@ -133,32 +135,28 @@ class Matcher:
             filter=filter_clause,
         )
         s = (
-            Search(using=self.es, index=self.index)
+            Search(using=self.es, index=index)
             .query(query)
             .sort({timestamp_field: {"order": "desc"}})
             .extra(size=lookback_size)
         )
-        result = self.query_index(s)
+        result = self.query_index(index, s)
         hits = result.hits.hits
         uuids_docs = []
         for hit in hits:
             if "buildUrl" in hit["_source"]:
-                uuids_docs.append(
-                    {
+                uuids_docs.append({
                         self.uuid_field: hit.to_dict()["_source"][self.uuid_field],
                         "buildUrl": hit.to_dict()["_source"]["buildUrl"],
-                    }
-                )
+                        "ocpVersion": hit.to_dict()["_source"]["ocpVersion"]  })
             else:
-                uuids_docs.append(
-                    {
+                uuids_docs.append({
                         self.uuid_field: hit.to_dict()["_source"][self.uuid_field],
                         "buildUrl": "http://bogus-url",
-                    }
-                )
+                        "ocpVersion": hit.to_dict()["_source"]["ocpVersion"]  })
         return uuids_docs
 
-    def match_kube_burner(self, uuids: List[str]) -> List[Dict[str, Any]]:
+    def match_kube_burner(self, uuids: List[str], index: str) -> List[Dict[str, Any]]:
         """match kube burner runs
         Args:
             uuids (list): list of uuids
@@ -174,11 +172,9 @@ class Matcher:
             ],
         )
         search = (
-            Search(using=self.es, index=self.index)
-            .query(query)
-            .extra(size=self.search_size)
+            Search(using=self.es, index=index).query(query).extra(size=self.search_size)
         )
-        result = self.query_index(search)
+        result = self.query_index(index, search)
         runs = [item.to_dict()["_source"] for item in result.hits.hits]
         return runs
 
@@ -199,9 +195,10 @@ class Matcher:
         ids_df = ndf.loc[df["jobConfig.jobIterations"] == iterations]
         return ids_df["uuid"].to_list()
 
-    def get_results(
+    def getResults(
         self, uuid: str,
         uuids: List[str],
+        index_str: str,
         metrics: Dict[str, Any]
     ) -> Dict[Any, Any]:
         """
@@ -210,6 +207,7 @@ class Matcher:
         Args:
             uuid (str): _description_
             uuids (list): _description_
+            index_str (str): _description_
             metrics (dict): _description_
 
         Returns:
@@ -236,23 +234,24 @@ class Matcher:
             ],
         )
         search = (
-            Search(using=self.es, index=self.index)
+            Search(using=self.es, index=index_str)
             .query(query)
             .extra(size=self.search_size)
         )
-        result = self.query_index(search)
+        result = self.query_index(index_str, search)
         runs = [item.to_dict()["_source"] for item in result.hits.hits]
         return runs
 
     def get_agg_metric_query(
         self, uuids: List[str],
+        index: str,
         metrics: Dict[str, Any],
-        timestamp_field: str = "timestamp"
-    ):
+        timestamp_field: str="timestamp"):
         """burner_metric_query will query for specific metrics data.
 
         Args:
             uuids (list): List of uuids
+            index (str): ES/OS Index to query from
             metrics (dict): metrics defined in es index metrics
         """
         metric_queries = []
@@ -269,14 +268,12 @@ class Matcher:
         query = Q(
             "bool",
             must=[
-                Q("terms", **{self.uuid_field + ".keyword": uuids}),
+                Q("terms", **{self.uuid_field +".keyword": uuids}),
                 metric_query,
             ],
         )
         search = (
-            Search(using=self.es, index=self.index)
-            .query(query)
-            .extra(size=self.search_size)
+            Search(using=self.es, index=index).query(query).extra(size=self.search_size)
         )
         agg_value = metrics["agg"]["value"]
         agg_type = metrics["agg"]["agg_type"]
@@ -286,7 +283,7 @@ class Matcher:
         search.aggs.bucket(
             "uuid", "terms", field=self.uuid_field+".keyword", size=self.search_size
         ).metric(agg_value, agg_type, field=metrics["metric_of_interest"])
-        result = self.query_index(search)
+        result = self.query_index(index, search)
         data = self.parse_agg_results(result, agg_value, agg_type, timestamp_field)
         return data
 
@@ -294,7 +291,7 @@ class Matcher:
         self, data: Dict[Any, Any],
         agg_value: str,
         agg_type: str,
-        timestamp_field: str = "timestamp"
+        timestap_field: str="timestamp"
     ) -> List[Dict[Any, Any]]:
         """parse out CPU data from kube-burner query
         Args:
@@ -311,7 +308,7 @@ class Matcher:
         for stamp in stamps:
             dat = {}
             dat[self.uuid_field] = stamp.key
-            dat[timestamp_field] = stamp.time.value_as_string
+            dat[timestap_field] = stamp.time.value_as_string
             agg_values = next(
                 (item for item in agg_buckets if item.key == stamp.key), None
             )
@@ -325,7 +322,7 @@ class Matcher:
     def convert_to_df(
         self, data: Dict[Any, Any],
         columns: List[str] = None,
-        timestamp_field: str = "timestamp"
+        timestamp_field: str="timestamp"
     ) -> pd.DataFrame:
         """convert to a dataframe
         Args:
